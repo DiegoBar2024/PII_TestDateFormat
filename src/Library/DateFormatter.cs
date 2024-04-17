79627a999c645860d9c82f2ac3202cd72a3382fd
DiegoBar2024 <diego.barboza@correo.ucu.edu.uy>
2024-04-17 20:54:05 -0300

    Algunos cambios

--- src/Library/DateFormatter.cs
+++ src/Library/DateFormatter.cs
@@ -1,4 +1,6 @@
-﻿namespace Ucu.Poo.TestDateFormat;
+﻿using System.Text;
+
+namespace Ucu.Poo.TestDateFormat;
 
 /// <summary>
 /// Esta clase implementa la funcionalidad de cambiar el formato de una fecha.
@@ -14,7 +16,45 @@ public class DateFormatter
     /// <param name="date">Una fecha en formato "dd/mm/yyyy".</param>
     /// <returns>La fecha convertida al formato "yyyy-mm-dd".</returns>
     public static string ChangeFormat(string date)
+    // OPCIÓN 1:
+    //     StringBuilder newDate = new StringBuilder();
+    //     bool isValid = true;
+
+    //     if (String.IsNullOrEmpty(date))
+    //     {
+    //         newDate.Append("La fecha no puede ser vacía");
+    //         isValid = false;
+    //     }
+
+    //     else if (date.Length != 10)
+    //     {
+    //         newDate.Append("La longitud de la fecha tiene que tener 10 caracteres");
+    //         isValid = false;
+    //     }
+
+    //     if (isValid)
+    //     {
+    //         newDate.Append(date.Substring(6) + "-" + date.Substring(3, 2) + "-" + date.Substring(0, 2));
+    //     }
+
+    //     return newDate.ToString();
+
+    // }
+    // OPCIÓN 2:
     {
+        // En caso de que la fecha de entrada sea vacía, que me avise
+        if (String.IsNullOrEmpty(date))
+        {
+            return "La fecha no puede ser vacía";
+        }
+
+        // En caso que la longitud de la fecha de entrada sea diferente de 10, que me avise
+        if (date.Length != 10)
+        {
+            return "La longitud de la fecha tiene que tener 10 caracteres";
+        }
+
+        // En caso que la fecha cumpla con las restricciones de longitud, que me cambie el formato
         return date.Substring(6) + "-" + date.Substring(3, 2) + "-" + date.Substring(0, 2);
     }
 }
---
+++ tests/Library.Tests/DataFormatterTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using Ucu.Poo.TestDateFormat;
+
+namespace Library.Tests
+{
+    public class DataFormatterTests
+    {
+
+        // Creo un test para la fecha correcta
+        [Test]
+        public void TestCorrectDate()
+        {
+            const string myDate = "02/04/2002";
+            const string dateExpected = "2002-04-02";
+
+            string newFormatDate = DateFormatter.ChangeFormat(myDate);
+
+            Assert.AreEqual(dateExpected, newFormatDate);
+        }
+
+        // Creo un test para chequear la fecha incorrecta
+        [Test]
+        public void TestIncorrectDate()
+        {
+            const string myDate = "2/04/2002";
+            const string dateExpected = "2002-04-2";
+
+            string newFormatDate = DateFormatter.ChangeFormat(myDate);
+
+            Assert.AreEqual(dateExpected, newFormatDate);
+        }
+
+        // Creo un test para chequear la fecha en blanco
+        [Test]
+        public void TestBlankDate()
+        {
+            const string myDate = "";
+            const string dateExpected = "";
+
+            string newFormatDate = DateFormatter.ChangeFormat(myDate);
+
+            Assert.AreEqual(dateExpected, newFormatDate);
+        }
+    }
+}
